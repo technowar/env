369e5b02227e5410fd4716c7ce8a21889a735052
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2014-09-17 20:50:25 +0800

    Added dependencies

---
+++ setup/bootstrap.sh
@@ -0,0 +1,22 @@
+apt-get update -y -q
+
+apt-get install vim
+apt-get install git
+apt-get install rake
+apt-get install htop
+apt-get install mongodb
+
+gem install hub
+
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install nodejs
+
+npm install -g bower
+npm install -g express
+npm install -g express-generator
+npm install -g grunt
+npm install -g grunt-cli
+npm install -g hotnode
+npm install -g mongoose
+npm install -g webit
+npm install -g yo
