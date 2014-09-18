e633b10e41cfe723243a3a9174554e4a80ce6476
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2014-09-18 08:10:28 +0800

    Added nodejs

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -1,5 +1,6 @@
 apt-get update -y -q
 
+apt-get install build-essential
 apt-get install vim
 apt-get install git
 apt-get install rake
@@ -8,8 +9,12 @@ apt-get install mongodb
 
 gem install hub
 
-curl -sL https://deb.nodesource.com/setup | sudo bash -
-apt-get install nodejs
+wget --quiet http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz
+tar -zxf node-v0.10.26-linux-x64.tar.gz
+
+mv node-v0.10.26-linux-x64/ /opt/node/
+ln -s /opt/node/bin/node /usr/bin/node
+ln -s /opt/node/bin/npm /usr/bin/npm
 
 npm install -g bower
 npm install -g express
